cd0acb40f779d208507e087e27be62b257154647
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-13 17:19:49 +0200

    Filter games by platforms

--- game-hub/src/components/PlatformSelector.tsx
+++ game-hub/src/components/PlatformSelector.tsx
@@ -1,9 +1,15 @@
+import { Platform } from '@/hooks/useGames'
 import usePlatforms from '@/hooks/usePlatforms'
 import { BsChevronDown } from 'react-icons/bs'
 import { Button } from './ui/button'
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from './ui/menu'
 
-const PlatformSelector = () => {
+interface Props {
+    onSelectPlatform: (platform: Platform) => void
+    selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
     const { data, error } = usePlatforms();
     if (error) return null;
 
@@ -12,17 +18,20 @@ const PlatformSelector = () => {
             <MenuTrigger asChild>
                 <Button variant="outline" size="sm">
                     Platforms
+                    {selectedPlatform?.name || "Platforms"}
                     <BsChevronDown />
                 </Button>
             </MenuTrigger>
             <MenuContent>
                 {data.map(platform =>
-                    <MenuItem key={platform.id} value="new-txt-a">
+                    <MenuItem
+                        key={platform.id}
+                        value="new-txt-a"
+                        onClick={() => onSelectPlatform(platform)}
+                    >
                         {platform.name}
                     </MenuItem>
                 )}
-
-
             </MenuContent>
         </MenuRoot>
     )
