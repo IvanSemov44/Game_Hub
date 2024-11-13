b83237a3ebb2c19005bc7b6486a4b514b1621f42
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-13 19:49:32 +0200

    Refactor GameGrid

---
+++ game-hub/src/components/data/genres.ts
@@ -0,0 +1,46 @@
+export default {
+  id: 4,
+  name: "Action",
+  slug: "action",
+  games_count: 183494,
+  image_background:
+    "https://media.rawg.io/media/games/4be/4be6a6ad0364751a96229c56bf69be59.jpg",
+  games: [
+    {
+      id: 3498,
+      slug: "grand-theft-auto-v",
+      name: "Grand Theft Auto V",
+      added: 21345,
+    },
+    {
+      id: 3328,
+      slug: "the-witcher-3-wild-hunt",
+      name: "The Witcher 3: Wild Hunt",
+      added: 20864,
+    },
+    {
+      id: 5286,
+      slug: "tomb-raider",
+      name: "Tomb Raider (2013)",
+      added: 17001,
+    },
+    {
+      id: 13536,
+      slug: "portal",
+      name: "Portal",
+      added: 16821,
+    },
+    {
+      id: 12020,
+      slug: "left-4-dead-2",
+      name: "Left 4 Dead 2",
+      added: 16712,
+    },
+    {
+      id: 5679,
+      slug: "the-elder-scrolls-v-skyrim",
+      name: "The Elder Scrolls V: Skyrim",
+      added: 16065,
+    },
+  ],
+};
