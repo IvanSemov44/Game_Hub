0aa05e7771c1f8c6cb77936c738eebc35aebe526
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-13 14:08:02 +0200

    Show loading skeleton

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
     return (
-        <Card.Root key={game.id} maxW="md" borderRadius={10} overflow="hidden">
+        <Card.Root width="300px" key={game.id} maxW="md" borderRadius={10} overflow="hidden">
             <Image src={getCroppedImageUrl(game.background_image)} />
             <Card.Body>
                 <Heading fontSize="2xl">{game.name}</Heading>
