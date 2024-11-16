a8fc6c40633830fd15ff4211c4512095eca3877b
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-16 10:23:02 +0200

    Refactoring: remove duplicate interfaces

--- game-hub/src/components/PlatformIconList.tsx
+++ game-hub/src/components/PlatformIconList.tsx
@@ -1,4 +1,4 @@
-import { Platform } from '@/hooks/useGames';
+import { Platform } from '@/hooks/usePlatforms';
 import { HStack } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import { BsGlobe } from "react-icons/bs";
