98ade4ce6ad3f98807318ca23de89f1f96d1f925
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-19 11:55:53 +0200

    build game attribute

---
+++ game-hub/src/components/GameAttributes.tsx
@@ -0,0 +1,38 @@
+import { Game } from '@/entities/Game';
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import CriticScore from './CriticScore';
+import DefinictionItem from './DefinictionItem';
+
+interface Props {
+    game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+    return (
+        <SimpleGrid columns={2} as="dl">
+            <DefinictionItem term="Platforms">
+                {game.parent_platforms?.map(({ platform }) =>
+                    <Text key={platform.id}>{platform.name}</Text>
+                )}
+            </DefinictionItem>
+
+            <DefinictionItem term="Metascores">
+                <CriticScore score={game.metacritic} />
+            </DefinictionItem>
+
+            <DefinictionItem term="Genres">
+                {game.genres.map((genre) =>
+                    <Text key={genre.id}>{genre.name}</Text>
+                )}
+            </DefinictionItem>
+
+            <DefinictionItem term="Publisher">
+                {game.publishers?.map(publisher =>
+                    <Text key={publisher.id}>{publisher.name}</Text>
+                )}
+            </DefinictionItem>
+        </SimpleGrid>
+    )
+}
+
+export default GameAttributes
--- game-hub/src/pages/GameDetailPage.tsx
+++ game-hub/src/pages/GameDetailPage.tsx
@@ -1,4 +1,5 @@
 import ExpandableText from "@/components/ExpandableText";
+import GameAttributes from "@/components/GameAttributes";
 import useGame from "@/hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
@@ -7,15 +8,18 @@ const GameDetailPage = () => {
     const { slug } = useParams();
     const { data: game, isLoading, error } = useGame(slug!);
 
-    if (isLoading) return <Spinner />
-    if (error || !game) throw error
+    if (isLoading) return <Spinner />;
+    if (error || !game) throw error;
 
     return (
         <>
             <Heading>{game.name}</Heading>
-            <ExpandableText>{game.description_raw}</ExpandableText>
-        </>
-    )
-}
 
-export default GameDetailPage
+            <ExpandableText>{game.description_raw}</ExpandableText>
+
+            <GameAttributes game={game} />
+        </>
+    );
+};
+
+export default GameDetailPage;
