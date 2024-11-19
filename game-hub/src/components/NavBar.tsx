39b3869f1c460f777ec708a7eac461e461baea2c
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-19 12:41:51 +0200

    Fix the NavBar

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { ColorModeButton } from "@/components/ui/color-mode";
 import { HStack, Image } from '@chakra-ui/react';
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
@@ -7,7 +8,9 @@ import SearchInput from "./SearchInput";
 const NavBar = () => {
     return (
         <HStack>
-            <Image src={logo} boxSize="60px" />
+            <Link to="/">
+                <Image src={logo} boxSize="60px" objectFit={"cover"} />
+            </Link>
             <SearchInput />
             <HStack padding="10px" >
                 <ColorModeButton />
--- game-hub/src/components/SearchInput.tsx
+++ game-hub/src/components/SearchInput.tsx
@@ -2,17 +2,20 @@ import useGameQueryStore from '@/store';
 import { Input } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 import { InputGroup } from './ui/input-group';
 
 const SearchInput = () => {
     const ref = useRef<HTMLInputElement>(null);
     const setSearchText = useGameQueryStore(s => s.setSearchText);
+    const navigate = useNavigate();
 
     return (
         <form
             onSubmit={(event => {
                 event.preventDefault();
                 setSearchText(ref.current!.value);
+                navigate("/")
             })}>
 
             <InputGroup
